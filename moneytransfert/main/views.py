96150990c3264a5caccee8430a9b51f5252df914
Hugrisse <fusil.dawei@gmail.com>
2024-03-30 02:55:22 +0800

    updates

--- moneytransfert/main/views.py
+++ moneytransfert/main/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
+                return redirect('signin')  # Redirect to the sign-in page after successful sign-up
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to the home page after successful sign-in
+    return render(request, 'signin.html')
+
+
