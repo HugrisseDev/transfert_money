e99b8882374584727aa1ad494d25517ae31e3741
Hugrisse <fusil.dawei@gmail.com>
2024-04-02 00:13:52 +0800

    some updates

--- moneytransfert/main/models.py
+++ moneytransfert/main/models.py
@@ -21,6 +21,3 @@ class TransactionHistory(models.Model):
     def __str__(self):
         return f"{self.user.username} {self.action} {self.transaction.amount} {self.transaction.currency} on {self.date_time}"
 
-
-class login:
-    pass
